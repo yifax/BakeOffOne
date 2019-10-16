9da315e04ff4e85b65e79d91f8f9a56917c6c14b
yifax <yifax10@uci.edu>
2019-10-15 19:53:25 -0700

    做好了转盘的内容

--- app/src/main/java/edu/ucla/hci/bakeoffone/MainActivity.java
+++ app/src/main/java/edu/ucla/hci/bakeoffone/MainActivity.java
@@ -2,26 +2,136 @@ package edu.ucla.hci.bakeoffone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
     int layoutWidth, layoutHeight;
     int centerX;
     int centerY;
+    int MODE = 0;
     TextView Zero;
+    TextView NE;
     TextView Three;
+    TextView SE;
     TextView Six;
+    TextView SW;;
     TextView Nine;
+    TextView NW;
     TextView textView;
     Button redDot;
+
+    //Calculate the current clockwise-view angle
+    double angleCalculator(int cX, int cY, float X, float Y){
+        double radius = Math.atan2(X-cX, cY-Y);
+        return radius / (Math.PI / 180);
+    }
+
+    // Selection made, generate actual input
+    int selectionDetect(float currentX, float currentY){
+        double ang = angleCalculator(centerX, centerY, currentX, currentY);
+        Log.i("TAG", "angle:" + ang);
+        if ((-22.5 <= ang) && (ang < 22.5)) {    // Zero
+            return 1;
+        }
+        if ((22.5 <= ang) && (ang < 67.5)) {    // NE
+            return 2;
+        }
+        if ((67.5 <= ang) && (ang < 112.5)) {    // Three
+            return 3;
+        }
+        if ((112.5 <= ang) && (ang < 157.5)) {    // SE
+            return 4;
+        }
+        if ((157.5 <= ang) || ( ang > 157.5)) {    // Six
+            return 5;
+        }
+        if ((-157.5 <= ang) && (ang < -112.5)) {    // SW
+            return 6;
+        }
+        if ((-112.5 <= ang) && (ang < -67.5)) {    // Nine
+            return 7;
+        }
+        if ((-67.5 <= ang) && (ang < -22.5)) {    // NW
+            return  8;
+        }
+        return 0;
+    }
+
+    void refreshDisplay(int selection) {
+        int scale = 25;
+        int original = 20;
+        Zero.setTextSize(original);
+        NE.setTextSize(original);
+        Three.setTextSize(original);
+        SE.setTextSize(original);
+        Six.setTextSize(original);
+        SW.setTextSize(original);
+        Nine.setTextSize(original);
+        NW.setTextSize(original);
+        Zero.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        NE.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        Three.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        SE.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        Six.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        SW.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        Nine.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        NW.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        // Highlight Border of selected Input-Letter-Box.
+        switch (selection) {
+            case 1: Zero.setTextSize(scale); Zero.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 2: NE.setTextSize(scale); NE.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 3: Three.setTextSize(scale); Three.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 4: SE.setTextSize(scale); SE.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 5: Six.setTextSize(scale); Six.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 6: SW.setTextSize(scale); SW.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 7: Nine.setTextSize(scale); Nine.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 8: NW.setTextSize(scale); NW.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD)); break;
+            case 0: Log.i("TAG", "Angle Detection ERROR!");
+        }
+    }
+
+    void iniLetters(int selection){
+        String[] cc = new String[]{"X","X","X","X","X","X","X","X"};
+        switch (selection) {
+            case 1: cc = new String[]{"A","B","C","D","E","F"," "," "}; break;
+            case 2: cc = new String[]{"←"," "," "," "," "," "," "," "}; break;
+            case 3: cc = new String[]{"G","H","I","J","K","L","M","N"}; break;
+            case 4: cc = new String[]{"0","1","2","3","4","5","6","7"}; break;
+            case 5: cc = new String[]{"O","P","Q","R","S","T"," "," "}; break;
+            case 6: cc = new String[]{"8","9","!","?",",",".","/","-"}; break;
+            case 7: cc = new String[]{"U","V","W","X","Y","Z"," "," "}; break;
+            case 8: cc = new String[]{"⌴"," "," "," "," "," "," "," "}; break;
+            case 0: Log.i("TAG", "Angle Detection ERROR!");
+        }
+        Zero.setText(cc[0]);
+        NE.setText(cc[1]);
+        Three.setText(cc[2]);
+        SE.setText(cc[3]);
+        Six.setText(cc[4]);
+        SW.setText(cc[5]);
+        Nine.setText(cc[6]);
+        NW.setText(cc[7]);
+    }
+    void restoreLetters(){
+        String[] cc = new String[]{"A-F","<-","G-N","#","O-T","*","U-Z","⌴"};
+        Zero.setText(cc[0]);
+        NE.setText(cc[1]);
+        Three.setText(cc[2]);
+        SE.setText(cc[3]);
+        Six.setText(cc[4]);
+        SW.setText(cc[5]);
+        Nine.setText(cc[6]);
+        NW.setText(cc[7]);
+    }
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -32,51 +142,27 @@ public class MainActivity extends AppCompatActivity {
         centerY = layoutHeight / 2;
     }
 
-    // Selection made, generate actual input
-    void lightUp(int currentX, int currentY) {
-        int quarterX = centerX / 2;
-        int quarterY = centerY / 2;
-        Log.i("TAG", "3/2x:" + quarterX + " 3/2y:" + quarterY + " currentX" + currentX + " currentY" + currentY);
-        if ((currentY < centerY) && (quarterX < currentX) && (currentX < 3*quarterX/2)){    // Activate Up
-            Zero.setImageDrawable(getResources().getDrawable((R.drawable.on)));
-            Three.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Six.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Nine.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-        }
-        if ((currentX > centerX) && (quarterY < currentY) && (currentY < 3*quarterY/2)){    // Activate Left
-            Three.setImageDrawable(getResources().getDrawable((R.drawable.on)));
-            Six.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Nine.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Zero.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-        }
-        if ((currentY > centerY) && (quarterX < currentX) && (currentX < 3*quarterX/2)){    // Activate Down
-            Six.setImageDrawable(getResources().getDrawable((R.drawable.on)));
-            Nine.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Zero.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Three.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-        }
-        if ((currentX < centerX) && (quarterY < currentY) && (currentY < 3*quarterY/2)){    // Activate Right
-            Nine.setImageDrawable(getResources().getDrawable((R.drawable.on)));
-            Zero.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Three.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-            Six.setImageDrawable(getResources().getDrawable((R.drawable.off)));
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Zero = (TextView) findViewById(R.id.Zero);
+        NE = (TextView) findViewById(R.id.NE);
         Three = (TextView) findViewById(R.id.Three);
+        SE = (TextView) findViewById(R.id.SE);
         Six = (TextView) findViewById(R.id.Six);
+        SW = (TextView) findViewById(R.id.SW);
         Nine = (TextView) findViewById(R.id.Nine);
+        NW = (TextView) findViewById(R.id.NW);
         textView = (TextView) findViewById(R.id.textView);
         redDot = (Button) findViewById(R.id.redDot);
         redDot.setOnTouchListener(new View.OnTouchListener(){
             int lastX, lastY; //Save last location
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                int act=event.getAction();
+                int selection = 0;
+                float cX, cY;
+                int act = event.getAction();
                 switch(act){
                     case MotionEvent.ACTION_DOWN:
                         lastX = (int) event.getRawX();
@@ -107,13 +193,24 @@ public class MainActivity extends AppCompatActivity {
                             top = bottom - v.getHeight();
                         }
                         v.layout(left, top, right, bottom); // Draw new button
-                        lightUp(left,top);   // Highlight Border of selected Input-Letter-Box.
+                        cX = v.getX() + v.getWidth()  / 2;
+                        cY = v.getY() + v.getHeight() / 2;
+                        selection = selectionDetect(cX, cY);
+                        if (MODE == 0) {    // Initialize Input Mode
+                            MODE += 1;
+                            iniLetters(selection);
+                        }
+                        else {
+                            MODE -=1;
+                            restoreLetters();
+                        }
+                        refreshDisplay(selection);
                         // Toast.makeText(getActivity(), "position：" + left + ", " +
                         // top + ", " + right + ", " + bottom, 0)
                         // .show();
                         lastX = (int) event.getRawX();
                         lastY = (int) event.getRawY();
-                        textView.setText("DEBUG #: LEFT:" + left + " TOP:" + top +" x:" + lastX + " y:" + lastY);
+                        //textView.setText("DEBUG #: LEFT:" + left + " TOP:" + top +" x:" + lastX + " y:" + lastY);
                         break;
                     case MotionEvent.ACTION_UP: // Up
                         // Attach to the border
